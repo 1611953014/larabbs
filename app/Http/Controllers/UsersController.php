fd1c728b63de9a097bc1582e6e8eadee29be8792
老金 <1611953014@qq.com>
2020-08-03 17:35:06 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 
@@ -14,12 +15,23 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $aa = 1;
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar,'avatars',$user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 }
