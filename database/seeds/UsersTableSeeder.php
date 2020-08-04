35b35f23f51cea0367ccd9bbed21600c0d4a60c0
老金 <1611953014@qq.com>
2020-08-04 10:23:36 +0800

    填充用户和话题数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(10)->create();
+
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'Summer@example.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
