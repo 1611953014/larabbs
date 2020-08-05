f1bd85e77f1fe3d12430ed6849974a8e47ff1449
老金 <1611953014@qq.com>
2020-08-05 10:32:35 +0800

    回复数据7.1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+
         //话题观察者注册
         Topic::observe(TopicObserver::class);
     }
---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,18 @@
+<?php
+///** @var Factory $factory */
+
+use Faker\Generator as Faker;
+//use Illuminate\Database\Eloquent\Factory;
+
+$factory->define(App\Models\Reply::class, function (Faker $faker) {
+    //随机取一个月内的时间
+    $time = $faker->dateTimeThisMonth();
+
+    return [
+        'content'=>$faker->sentence(),
+        'created_at' => $time,
+        'updated_at'=>$time,
+        'topic_id' => rand(1, 100),
+        'user_id' => rand(1, 10)
+    ];
+});
