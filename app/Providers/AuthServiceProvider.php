e685e5423561f6ca7e9a59e3d37fb890545409b0
老金 <1611953014@qq.com>
2020-08-06 10:09:27 +0800

    集成administrator

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -32,9 +33,9 @@ class AuthServiceProvider extends ServiceProvider
             return 'App\Policies\\'.class_basename($modelClass).'Policy';
         });
 
-        \Horizon::auth(function ($request) {
+        /*Horizon::auth(function ($request) {
             // 是否是站长
-            return \Auth::user()->hasRole('Founder');
-        });
+            return Auth::user()->hasRole('Founder');
+        });*/
     }
 }
