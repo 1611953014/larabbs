1627b6be9b8820c1471eb84b79e708b055dae66d
高槻泉 <1611953014@qq.com>
2020-08-09 14:55:58 +0800

    图片验证码

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,9 @@ Route::prefix('v1')->namespace('Api')
     ->group(function() {
         Route::middleware('throttle:' . config('api.rate_limits.sign'))
             ->group(function(){
+                // 图片验证码
+                Route::post('captchas', 'CaptchasController@store')
+                    ->name('captchas.store');
                 //短信验证码
                 Route::post('verificationCodes', 'verificationCodesController@store')
                     ->name('verification.store');
