b0ae726e37b1da1257e2e4691f26333a33c20447
高槻泉 <1611953014@qq.com>
2020-08-09 14:30:18 +0800

    用户注册3.4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Topic;
 use App\Observers\TopicObserver;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,5 +32,7 @@ class AppServiceProvider extends ServiceProvider
         \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
         \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+
+        JsonResource::withoutWrapping();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,10 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 });*/
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
+    //短信验证码
     Route::post('verificationCodes', 'verificationCodesController@store')
         ->name('verification.store');
+    Route::post('users', 'UsersController@store')
+        ->name('users.store');
 });
 
